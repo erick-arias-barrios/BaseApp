3e68cf5ed288af44fab9bf193a406e318b3a609f
Erick Arias <aberick4@misena.edu.co>
2020-11-08 16:22:03 -0500

    Add project files.

---
+++ BaseApp/BaseApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using BaseApp.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace BaseApp.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public Command LoginCommand { get; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new Command(OnLoginClicked);
+        }
+
+        private async void OnLoginClicked(object obj)
+        {
+            // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
+            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+        }
+    }
+}
---
+++ BaseApp/BaseApp/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BaseApp.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
